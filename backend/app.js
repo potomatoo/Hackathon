cf095e50c8b6fe228afa0475d5df21aea3df02f2
ORANZINO <hupajupa61@gmail.com>
2021-05-13 19:43:10 +0000

    error fix

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
+const cors = require('cors');
 
 var app = express();
 
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -17,6 +19,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+  });
 
 app.use('/', indexRouter);
 
--- backend/controllers/dogController.js
+++ backend/controllers/dogController.js
@@ -6,14 +6,14 @@ const DogModel = require('../models/dogs');
 module.exports = {
     getFeatureDogs: async (req, res) => {
         // 데이터 받아오기
-        let feature = req.feature;
+		const feature = req.query.feature;
         // data check - undefined
-        if (!feature) {
-            return res.status(CODE.OK).send(util.success(CODE.BAD_REQUEST, MSG.NULL_VALUE));
+        if (feature === undefined) {
+            return res.status(CODE.BAD_REQUEST).send(util.success(CODE.BAD_REQUEST, MSG.NULL_VALUE));
         } 
         try {
             // call model - database
-            const result = await DogModel.getFeatureDogs(feature);
+            let result = await DogModel.getFeatureDogs(feature);
             for (var i = 0; i < result.length; i++) {
                 result[i]['image'] = `https://helpdogs.s3.ap-northeast-2.amazonaws.com/homeless/${result[i]['dog_idx']}.jpg`;
                 delete result[i].dog_idx;
