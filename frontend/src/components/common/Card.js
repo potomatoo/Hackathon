ca14258bf4007a97a4ad620cc62d2136058ce52b
yeonjae <duseh73@gmail.com>
2021-05-14 03:32:56 +0900

    add winner

--- frontend/src/components/common/Card.js
+++ frontend/src/components/common/Card.js
@@ -1,100 +1,100 @@
-import React, { useState } from "react";
-import styled, { css } from "styled-components";
-import palette from "../../lib/style/palette";
+import React, { useState } from 'react';
+import styled, { css } from 'styled-components';
+import palette from '../../lib/style/palette';
 
 const CardBlock = styled.div`
-  width: 250px;
-  height: 250px;
-  left: 420px;
-  top: 296px;
+	width: 250px;
+	height: 250px;
+	left: 420px;
+	top: 296px;
 
-  margin-right: ${(marginlr) => "2rem"};
-  margin-left: ${(marginlr) => "2rem"};
+	margin-right: ${marginlr => '2rem'};
+	margin-left: ${marginlr => '2rem'};
 
-  background-color: ${palette.white};
-  box-shadow: 0 0px 50px 0 rgba(0, 0, 0, 0.25);
-  transition: 0.5s;
-  border-radius: 5px;
+	background-color: ${palette.white};
+	box-shadow: 0 0px 50px 0 rgba(0, 0, 0, 0.25);
+	transition: 0.5s;
+	border-radius: 5px;
 
-  &:hover {
-    box-shadow: 0 0px 100px 0 rgba(0, 0, 0, 0.15);
-  }
+	&:hover {
+		box-shadow: 0 0px 100px 0 rgba(0, 0, 0, 0.15);
+	}
 
-  &.selected {
-    width: 0%;
-    height: 0%;
-    opacity: 0;
-    font-size: 0 !important;
-    margin: 0rem;
-  }
+	&.selected {
+		width: 0%;
+		height: 0%;
+		opacity: 0;
+		font-size: 0 !important;
+		margin: 0rem;
+	}
 
-  ${({ marginlr }) =>
-    marginlr &&
-    css`
-      margin-left: 2rem;
-      margin-right: 2rem;
-    `}
+	${({ marginlr }) =>
+		marginlr &&
+		css`
+			margin-left: 2rem;
+			margin-right: 2rem;
+		`}
 `;
 
 const DogPictureBlock = styled.div`
-  height: 80%;
-  background-image: url("https://dispatch.cdnser.be/wp-content/uploads/2018/08/745ab6ce176fa4b59d1af29bc91fa409.png");
-  background-size: cover;
-  background-position: center;
-  background-repeat: no-repeat;
-  border-radius: 5px;
-  cursor: pointer;
+	height: 80%;
+	background-image: url('https://dispatch.cdnser.be/wp-content/uploads/2018/08/745ab6ce176fa4b59d1af29bc91fa409.png');
+	background-size: cover;
+	background-position: center;
+	background-repeat: no-repeat;
+	border-radius: 5px;
+	cursor: pointer;
 `;
 
 const CardTextBlock = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 20%;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	height: 20%;
 `;
 
 const IconBlock = styled.div`
-  padding-right: 1rem;
-  cursor: pointer;
+	padding-right: 1rem;
+	cursor: pointer;
 `;
 
 const DogNameBlock = styled.div`
-  font-size: 1.125rem;
-  font-weight: bold;
-  color: ${palette.gray[8]};
+	font-size: 1.125rem;
+	font-weight: bold;
+	color: ${palette.gray[8]};
 
-  &.selected {
-    width: 0%;
-    height: 0%;
-    opacity: 0;
-    font-size: 0 !important;
-    margin: 0rem;
-  }
+	&.selected {
+		width: 0%;
+		height: 0%;
+		opacity: 0;
+		font-size: 0 !important;
+		margin: 0rem;
+	}
 `;
 
 export default function Card({ isDisappear, marginlr }) {
-  const [liked, setLiked] = useState(false);
-  const [show, setShow] = useState(true);
+	const [liked, setLiked] = useState(false);
+	const [show, setShow] = useState(true);
 
-  const onToggle = (e) => {
-    const temp = isDisappear;
-    isDisappear = false;
-    setLiked(!liked);
-    isDisappear = temp;
-  };
+	const onToggle = e => {
+		const temp = isDisappear;
+		isDisappear = false;
+		setLiked(!liked);
+		isDisappear = temp;
+	};
 
-  return (
-    <CardBlock
-      onClick={() => (isDisappear ? setShow(false) : "")}
-      className={show ? "" : "selected"}
-      marginlr={marginlr}
-    >
-      <DogPictureBlock />
-      <CardTextBlock>
-        <DogNameBlock />
-        <DogNameBlock>절미</DogNameBlock>
-        <IconBlock></IconBlock>
-      </CardTextBlock>
-    </CardBlock>
-  );
+	return (
+		<CardBlock
+			onClick={() => (isDisappear ? setShow(false) : '')}
+			className={show ? '' : 'selected'}
+			marginlr={marginlr}
+		>
+			<DogPictureBlock />
+			<CardTextBlock>
+				<DogNameBlock />
+				<DogNameBlock>절미</DogNameBlock>
+				<IconBlock></IconBlock>
+			</CardTextBlock>
+		</CardBlock>
+	);
 }
---
+++ frontend/src/pages/getCategory.js
@@ -0,0 +1,24 @@
+const category = [
+	'갓난이',
+	'개구쟁이',
+	'까망이',
+	'까칠이',
+	'듬직이',
+	'황검이',
+	'바둑이',
+	'사나이',
+	'더벅이',
+	'온순이',
+	'쪼꼬미',
+	'쫑긋이',
+	'크림이',
+	'허스키 느낌',
+	'황돌이',
+	'흰둥이'
+];
+
+function getCategory(cateNo) {
+	return category[cateNo];
+}
+
+export default getCategory;
